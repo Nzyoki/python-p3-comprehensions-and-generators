1837d52e34dd5886602693da020a7376bc5455ae
Scolastica Nzyoki <solanzioki18@gmail.com>
2025-02-24 00:25:32 +0300

    lists

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-
+#Using a list comprehension, write a function return_evens() that returns a list of all of the even elements of a sequence of integers.
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
+        
+    
+    
 
+    pass
+#Using a list comprehension, write a function make_exclamation() that takes a list of sentence strings and returns a list of sentence strings with exclamation marks at the end.
 def make_exclamation(sentence_list):
+    return [sentence + '!' for sentence in sentence_list]
     pass
